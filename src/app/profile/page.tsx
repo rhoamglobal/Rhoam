9b41adadad6d89a0ea11f83d9bc8a94f760f5ce7
rhoamglobal <rhoam.global@gmail.com>
2026-07-10 12:58:50 +0100

    update

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -10,9 +10,9 @@ import {
 } from "lucide-react";
 import {
   FaInstagram,
-  FaFacebook,
+
   FaXTwitter,
-  FaWhatsapp,
+
 } from "react-icons/fa6";
 import { useState, useEffect } from "react";
 
